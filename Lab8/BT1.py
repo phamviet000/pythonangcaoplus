cdd7453ea8930b69bc50fdfbbb950b631ae37ef5
phamviet000 <74182334+phamviet000@users.noreply.github.com>
2021-11-09 18:28:44 +0700

    upload
    con it loi :((

---
+++ Lab8/BT1.py
@@ -0,0 +1,12 @@
+import urllib.request
+import json
+url_google = urllib.request.urlopen('https://www.google.com/')
+content = url_google.read()
+print(url_google.read())
+f=open('D:\pythonnangcao\gitpy\pythonangcaoplus\Lab8\json1.txt','a',encoding='utf-8')
+#f.write(sitrng)
+f.close()
+
+
+
+
